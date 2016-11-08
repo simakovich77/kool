23165ba0a0f15f2d276b3f840d5ab17f85ed7dd8
simakovich77 <simakovich77@gmail.com>
2016-11-08 06:06:52 +0300

    jkhkhkjhjkhkj

---
+++ wp-content/themes/kool/header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <!--
+    Kool Store Template
+    http://www.templatemo.com/preview/templatemo_428_kool_store
+    -->
+    <meta charset="utf-8">
+    <title>Kool Store - Responsive eCommerce Template</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/bootstrap.css"?>>
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/normalize.min.css"?>>
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/font-awesome.min.css"?>>
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/animate.css"?>>
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/templatemo-misc.css"?>>
+    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/templatemo-style.css"?>>
+
+    <script src=<?php bloginfo(template_url); echo "/js/vendor/modernizr-2.6.2.min.js"; ?>></script>
+
+</head>
+<body>
+
+
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
+
+
+<header class="site-header">
+    <div class="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="top-header-left">
+                        <a href=<?php echo site_url(); echo "/wp-login.php"?>>Sign Up</a>
+                        <a href=<?php echo site_url(); echo "/wp-login.php"?>>Log In</a>
+                    </div> <!-- /.top-header-left -->
+                </div> <!-- /.col-md-6 -->
+                <div class="col-md-6 col-sm-6">
+                    <div class="social-icons">
+                        <ul>
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <li><a href="#" class="fa fa-dribbble"></a></li>
+                            <li><a href="#" class="fa fa-twitter"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div> <!-- /.social-icons -->
+                </div> <!-- /.col-md-6 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.top-header -->
+    <div class="main-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-8">
+                    <div class="logo">
+                        <h1><a href=<?php echo site_url(); echo "/index.php";?>>Kool Store</a></h1>
+                    </div> <!-- /.logo -->
+                </div> <!-- /.col-md-4 -->
+                <div class="col-md-8 col-sm-6 col-xs-4">
+                    <div class="main-menu">
+                        <a href="#" class="toggle-menu">
+                            <i class="fa fa-bars"></i>
+                        </a>
+                        <ul class="menu">
+                            <li><a href=<a href=<?php echo site_url(); echo "/index.php"?>>Home</a></li>
+                            <li><a href="#">Catalogs</a></li>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Policies</a></li>
+                            <li><a href="#">About</a></li>
+                        </ul>
+                    </div> <!-- /.main-menu -->
+                </div> <!-- /.col-md-8 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.main-header -->
+    <div class="main-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-7">
+                    <div class="list-menu">
+                        <ul>
+                            <li><a href="index.html">Shop</a></li>
+                            <li><a href="product-detail.html">Details</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div> <!-- /.list-menu -->
+                </div> <!-- /.col-md-6 -->
+                <div class="col-md-6 col-sm-5">
+                    <div class="notification">
+                        <span>Free Shipping on any order above $50</span>
+                    </div>
+                </div> <!-- /.col-md-6 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.main-nav -->
+</header> <!-- /.site-header -->
--- wp-content/themes/kool/index.php
+++ wp-content/themes/kool/index.php
@@ -1,109 +1,20 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-<head>
-<!-- 
-Kool Store Template
-http://www.templatemo.com/preview/templatemo_428_kool_store
--->
-    <meta charset="utf-8">
-    <title>Kool Store - Responsive eCommerce Template</title>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
 
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet">
-
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/bootstrap.css"?>>
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/normalize.min.css"?>>
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/font-awesome.min.css"?>>
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/animate.css"?>>
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/templatemo-misc.css"?>>
-    <link rel="stylesheet" href=<?php bloginfo(template_url); echo "/css/templatemo-style.css"?>>
-
-    <script src=<?php bloginfo(template_url); echo "js/vendor/modernizr-2.6.2.min.js"; ?>></script>
-
-</head>
-<body>
-
-
-    <!--[if lt IE 7]>
-    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-    <![endif]-->
-
-    
-    <header class="site-header">
-        <div class="top-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-6">
-                        <div class="top-header-left">
-                            <a href=<?php echo site_url(); echo "/wp-login.php"?>>Sign Up</a>
-                            <a href=<?php echo site_url(); echo "/wp-login.php"?>>Log In</a>
-                        </div> <!-- /.top-header-left -->
-                    </div> <!-- /.col-md-6 -->
-                    <div class="col-md-6 col-sm-6">
-                        <div class="social-icons">
-                            <ul>
-                                <li><a href="#" class="fa fa-facebook"></a></li>
-                                <li><a href="#" class="fa fa-dribbble"></a></li>
-                                <li><a href="#" class="fa fa-twitter"></a></li>
-                                <li><a href="#" class="fa fa-linkedin"></a></li>
-                            </ul>
-                            <div class="clearfix"></div>
-                        </div> <!-- /.social-icons -->
-                    </div> <!-- /.col-md-6 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.top-header -->
-        <div class="main-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-6 col-xs-8">
-                        <div class="logo">
-                            <h1><a href=<?php echo site_url(); echo "/index.php";?>>Kool Store</a></h1>
-                        </div> <!-- /.logo -->
-                    </div> <!-- /.col-md-4 -->
-                    <div class="col-md-8 col-sm-6 col-xs-4">
-                        <div class="main-menu">
-                            <a href="#" class="toggle-menu">
-                                <i class="fa fa-bars"></i>
-                            </a>
-                            <ul class="menu">
-                                <li><a href=<a href=<?php echo site_url(); echo "/index.php"?>>Home</a></li>
-                                <li><a href="#">Catalogs</a></li>
-                                <li><a href="#">FAQs</a></li>
-                                <li><a href="#">Policies</a></li>
-                                <li><a href="#">About</a></li>
-                            </ul>
-                        </div> <!-- /.main-menu -->
-                    </div> <!-- /.col-md-8 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.main-header -->
-        <div class="main-nav">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-7">
-                        <div class="list-menu">
-                            <ul>
-                                <li><a href="index.html">Shop</a></li>
-                                <li><a href="product-detail.html">Details</a></li>
-                                <li><a href="contact.html">Contact</a></li>
-                            </ul>
-                        </div> <!-- /.list-menu -->
-                    </div> <!-- /.col-md-6 -->
-                    <div class="col-md-6 col-sm-5">
-                        <div class="notification">
-                            <span>Free Shipping on any order above $50</span>
-                        </div>
-                    </div> <!-- /.col-md-6 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.main-nav -->
-    </header> <!-- /.site-header -->
+get_header(); ?>
 
     <div class="content-section">
         <div class="container">
@@ -407,110 +318,7 @@ http://www.templatemo.com/preview/templatemo_428_kool_store
         </div> <!-- /.container -->
     </div> <!-- /.content-section -->
 
-    <footer class="site-footer">
-        <div class="our-partner">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="customNavigation">
-                            <a class="btn prev"><i class="fa fa-angle-left"></i></a>
-                            <a class="btn next"><i class="fa fa-angle-right"></i></a>
-                        </div>
-                        <div id="owl-demo" class="owl-carousel">
-                            <div class="item"> 
-                            	<a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-1.jpg";?> alt=""></a>
-                          	</div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-2.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-1.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-2.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-1.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-2.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-1.jpg"?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-2.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-1.jpg";?> alt=""></a>
-                            </div>
-                            <div class="item">
-                                <a href="#"><img src=<?php bloginfo(template_url); echo "/images/tm-170x80-2.jpg";?> alt=""></a>
-                            </div>
-                        </div> <!-- /#owl-demo -->
-                    </div> <!-- /.col-md-12 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.our-partner -->
-        <div class="main-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="footer-widget">
-                            <h3 class="widget-title">About Us</h3>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, debitis recusandae.
-                            <ul class="follow-us">
-                                <li><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
-                            </ul> <!-- /.follow-us -->
-                        </div> <!-- /.footer-widget -->
-                    </div> <!-- /.col-md-3 -->
-                    <div class="col-md-3">
-                        <div class="footer-widget">
-                            <h3 class="widget-title">Why Choose Us?</h3>
-                            Kool Store is free responsive eCommerce template provided by templatemo website. You can use this layout for any website.
-                            <br><br>Tempore cum mollitia eveniet laboriosam corporis voluptas earum voluptate. Lorem ipsum dolor sit amet.
-                            <br><br>Credit goes to <a rel="nofollow" href="http://unsplash.com">Unsplash</a> for all images.
-                        </div> <!-- /.footer-widget -->
-                    </div> <!-- /.col-md-3 -->
-                    <div class="col-md-2">
-                        <div class="footer-widget">
-                            <h3 class="widget-title">Useful Links</h3>
-                            <ul>
-                                <li><a href="#">Our Shop</a></li>
-                                <li><a href="#">Partners</a></li>
-                                <li><a href="#">Gift Cards</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Help</a></li>
-                            </ul>
-                        </div> <!-- /.footer-widget -->
-                    </div> <!-- /.col-md-2 -->
-                    <div class="col-md-4">
-                        <div class="footer-widget">
-                            <h3 class="widget-title">Our Newsletter</h3>
-                            <div class="newsletter">
-                                <form action="#" method="get">
-                                    <p>Sign up for our regular updates to know when new products are released.</p>
-                                    <input type="text" title="Email" name="email" placeholder="Your Email Here">
-                                    <input type="submit" class="s-button" value="Submit" name="Submit">
-                                </form>
-                            </div> <!-- /.newsletter -->
-                        </div> <!-- /.footer-widget -->
-                    </div> <!-- /.col-md-4 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.main-footer -->
-        <div class="bottom-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <span>Copyright &copy; 2084 <a href="#">Company Name</a> | Design: <a href="http://www.templatemo.com">templatemo</a></span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, expedita soluta mollitia accusamus ut architecto maiores cum fugiat. Pariatur ipsum officiis fuga deleniti alias quia nostrum veritatis enim doloremque eligendi?</p>
-                    </div> <!-- /.col-md-12 -->
-                </div> <!-- /.row -->
-            </div> <!-- /.container -->
-        </div> <!-- /.bottom-footer -->
-    </footer> <!-- /.site-footer -->
+<?php get_footer(); ?>
 
     
     <script src="js/vendor/jquery-1.10.1.min.js"></script>
