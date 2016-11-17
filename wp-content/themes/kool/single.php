42ce9cc60428a6f4723fea5416fe8832dff69723
sim.i.a <simakovich77@gmail.com>
2016-11-17 16:16:33 +0300

    dfgdfg1112312

--- wp-content/themes/kool/single.php
+++ wp-content/themes/kool/single.php
@@ -67,76 +67,7 @@ get_header(); ?>
 	</div> <!-- /.content-section -->
 
 
-
-
-
-
-
-
-
-				<div class="col-md-4">
-					<div class="footer-widget">
-						<h3 class="widget-title">Our Newsletter</h3>
-						<div class="newsletter">
-							<form action="#" method="get">
-								<p>Sign up for our regular updates to know when new products are released.</p>
-								<input type="text" title="Email" name="email" placeholder="Your Email Here">
-								<input type="submit" class="s-button" value="Submit" name="Submit">
-							</form>
-						</div> <!-- /.newsletter -->
-					</div> <!-- /.footer-widget -->
-				</div> <!-- /.col-md-4 -->
-			</div> <!-- /.row -->
-		</div> <!-- /.container -->
-	</div> <!-- /.main-footer -->
-
-
-
-
-
-
-
-
-
-	<div class="content-section">
-		<div class="container">
-			<div class="row">
-			<div class="col-md-8">
-
-		<div class="product-information">
-
-
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-
-
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-			</div>
-		</div>
-			</div>
-		</div>
-	</div>
-
-			
-		</div><!-- #content -->
-
-	</div><!-- #primary -->
-
 <?php
 
 get_footer();
+?>
