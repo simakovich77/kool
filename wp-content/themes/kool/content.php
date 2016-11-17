de8fcc8f7b4778a500efaae0ed31ed58b94ebf60
sim.i.a <simakovich77@gmail.com>
2016-11-17 16:12:22 +0300

    dfgdfg111231

--- wp-content/themes/kool/content.php
+++ wp-content/themes/kool/content.php
@@ -1,301 +1,71 @@
-<div class="content-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="product-item-1">
-                    <div class="product-thumb">
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
 
-                        <img src=<?php bloginfo(template_url); echo "/images/gallery-image-1.jpg"; ?> alt="Product Title">
-                    </div> <!-- /.product-thumb -->
-                    <div class="product-content">
-                        <h5><a href="#">Kool Shirt</a></h5>
-                        <span class="tagline">Partner Name</span>
-                        <span class="price">$25.00</span>
-                        <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt asperiores tenetur deleniti labore!</p>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-5">
-                <div class="product-holder">
-                    <div class="product-item-2">
-                        <div class="product-thumb">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php  ?>
 
-                            <img src=<?php bloginfo(template_url); echo "/images/featured/1.jpg";?> alt="Product Title">
-                        </div> <!-- /.product-thumb -->
-                        <div class="product-content overlay">
-                            <h5><a href="#">Kooler Shirt</a></h5>
-                            <span class="tagline">Partner Name</span>
-                            <span class="price">$30.00</span>
-                            <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
-                        </div> <!-- /.product-content -->
-                    </div> <!-- /.product-item-2 -->
-                    <div class="product-item-2">
-                        <div class="product-thumb">
-                            <img src=<?php bloginfo(template_url); echo "/images/featured/2.jpg";?> alt="Product Title">
-                        </div> <!-- /.product-thumb -->
-                        <div class="product-content overlay">
-                            <h5><a href="#">Koolest Shirt</a></h5>
-                            <span class="tagline">Partner Name</span>
-                            <span class="price">$45.00</span>
-                            <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
-                        </div> <!-- /.product-content -->
-                    </div> <!-- /.product-item-2 -->
-                    <div class="clearfix"></div>
-                </div> <!-- /.product-holder -->
-            </div> <!-- /.col-md-5 -->
-            <div class="col-md-4">
-                <div class="product-item-3">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/featured/6.jpg"?> alt="">
-                    </div> <!-- /.product-thumb -->
-                    <div class="product-content">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-6">
-                                <h5><a href="#">Name Of Shirt</a></h5>
-                                <span class="tagline">Partner Name</span>
-                                <span class="price">$20.00</span>
-                            </div> <!-- /.col-md-6 -->
-                            <div class="col-md-6 col-sm-6">
-                                <div class="full-row">
-                                    <label for="cat">Gender:</label>
-                                    <select name="cat" id="cat" class="postform">
-                                        <option value="-1">- Select -</option>
-                                        <option class="level-0" value="49">Female</option>
-                                        <option class="level-0" value="56">Male</option>
-                                    </select>
-                                </div>
-                                <div class="full-row">
-                                    <label for="cat1">Size:</label>
-                                    <select name="cat1" id="cat1" class="postform">
-                                        <option value="-1">- Select -</option>
-                                        <option class="level-0" value="49">Small</option>
-                                        <option class="level-0" value="49">Medium</option>
-                                        <option class="level-0" value="56">Large</option>
-                                        <option class="level-0" value="56">X-Large</option>
-                                    </select>
-                                </div>
-                                <div class="full-row">
-                                    <label for="cat2">Color:</label>
-                                    <select name="cat2" id="cat2" class="postform">
-                                        <option value="-1">- Select -</option>
-                                        <option class="level-0" value="2">Blue</option>
-                                        <option class="level-0" value="3">Red</option>
-                                        <option class="level-0" value="1">Pink</option>
-                                        <option class="level-0" value="4">Black</option>
-                                        <option class="level-0" value="4">Wlack</option>
-                                    </select>
-                                </div>
-                            </div> <!-- /.col-md-6 -->
-                            <div class="col-md-12 col-sm-12">
-                                <div class="button-holder">
-                                    <a href="#" class="red-btn"><i class="fa fa-angle-down"></i></a>
-                                </div> <!-- /.button-holder -->
-                            </div> <!-- /.col-md-12 -->
-                        </div> <!-- /.row -->
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-3 -->
-            </div> <!-- /.col-md-4 -->
-        </div> <!-- /.row -->
-        <div class="row">
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="product-item-4">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/featured/3.jpg";?> alt="Product Title">
-                    </div> <!-- /.product-thumb -->
-                    <div class="product-content overlay">
-                        <h5><a href="#">Name Of Shirt</a></h5>
-                        <span class="tagline">Partner Name</span>
-                        <span class="price">$60.00</span>
-                        <p>Doloremque quo possimus quas necessitatibus.</p>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-4 -->
-            </div> <!-- /.col-md-4 -->
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="product-item-4">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/featured/4.jpg";?> alt="Product Title">
-                    </div> <!-- /.product-thumb -->
-                    <div class="product-content overlay">
-                        <h5><a href="#">Name Of Shirt</a></h5>
-                        <span class="tagline">Partner Name</span>
-                        <span class="price">$30.00</span>
-                        <p>Doloremque quo possimus quas necessitatibus.</p>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-4 -->
-            </div> <!-- /.col-md-4 -->
-            <div class="col-md-4 col-sm-12 col-xs-12">
-                <div class="ads-placemenet">
-                    <img src=<?php bloginfo(template_url); echo "/images/featured/5.jpg";?> alt="">
-                </div> <!-- /.ads-placement -->
-            </div> <!-- /.col-md-4 -->
-        </div> <!-- /.row -->
-    </div> <!-- /.container -->
-</div> <!-- /.content-section -->
+	<header class="entry-header">
+		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) )  ) : ?>
+			<div class="entry-meta">
+				<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'kool' ) ); ?></span>
+			</div>
+			<?php
+		endif;
 
-<div class="content-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 section-title">
-                <h2>New Products</h2>
-            </div> <!-- /.section -->
-        </div> <!-- /.row -->
-        <div class="row">
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/gallery-image-2.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Name of Shirt</a></h5>
-                        <span class="price">$40.00</span>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/gallery-image-3.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Sport Shirt</a></h5>
-                        <span class="price">$40.00</span>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/gallery-image-4.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Elegant Shirt</a></h5>
-                        <span class="price">$60.00</span>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/gallery-image-5.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Another New Shirt</a></h5>
-                        <span class="price">$80.00</span>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item -->
-            </div> <!-- /.col-md-3 -->
-        </div> <!-- /.row -->
-    </div> <!-- /.container -->
-</div> <!-- /.content-section -->
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		endif;
+		?>
 
-<div class="content-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 section-title">
-                <h2>Vote For Future Products</h2>
-            </div> <!-- /.section -->
-        </div> <!-- /.row -->
-        <div class="row">
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item-vote">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/products/1.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Name of Shirt</a></h5>
-                        <span class="tagline">By: Catherine</span>
-                        <ul class="progess-bars">
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
-                                    <span>4<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                    <span class="comments">6<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-vote -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item-vote">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/products/2.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Name of Shirt</a></h5>
-                        <span class="tagline">By: Rebecca</span>
-                        <ul class="progess-bars">
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
-                                    <span>4<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                    <span class="comments">6<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-vote -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item-vote">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/products/3.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Name of Shirt</a></h5>
-                        <span class="tagline">By: Catherine</span>
-                        <ul class="progess-bars">
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
-                                    <span>4<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                    <span class="comments">6<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-vote -->
-            </div> <!-- /.col-md-3 -->
-            <div class="col-md-3 col-sm-6">
-                <div class="product-item-vote">
-                    <div class="product-thumb">
-                        <img src=<?php bloginfo(template_url); echo "/images/products/4.jpg";?> alt="">
-                    </div> <!-- /.product-thum -->
-                    <div class="product-content">
-                        <h5><a href="#">Name of Shirt</a></h5>
-                        <span class="tagline">By: Rebecca</span>
-                        <ul class="progess-bars">
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
-                                    <span>4<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="progress">
-                                    <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                    <span class="comments">6<i class="fa fa-heart"></i></span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div> <!-- /.product-content -->
-                </div> <!-- /.product-item-vote -->
-            </div> <!-- /.col-md-3 -->
-        </div> <!-- /.row -->
-    </div> <!-- /.container -->
-</div> <!-- /.content-section -->
+		<div class="entry-meta">
+			<?php
+			if ( 'post' == get_post_type() )
+
+
+			if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+				?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'kool' ), __( '1 Comment', 'kool' ), __( '% Comments', 'kool' ) ); ?></span>
+				<?php
+			endif;
+
+			edit_post_link( __( 'Edit', 'kool' ), '<span class="edit-link">', '</span>' );
+			?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<?php if ( is_search() ) : ?>
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	<?php else : ?>
+		<div class="entry-content">
+			<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'kool' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+			?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
+
+	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+</article><!-- #post-## -->
--- wp-content/themes/kool/single.php
+++ wp-content/themes/kool/single.php
@@ -9,8 +9,103 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+
+
+
+	<div class="content-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+
+					<div class="product-information">
+						<?php
+						// Start the Loop.
+						while ( have_posts() ) : the_post();
+
+							/*
+                             * Include the post format-specific template for the content. If you want to
+                             * use this in a child theme, then include a file called called content-___.php
+                             * (where ___ is the post format) and that will be used instead.
+                             */
+							get_template_part( 'content', get_post_format() );
+
+
+
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) {
+								comments_template();
+							}
+						endwhile;
+						?>
+					</div> <!-- /.product-information -->
+				</div> <!-- /.col-md-8 -->
+				<div class="col-md-4 col-sm-8">
+					<div class="product-item-2">
+						<div class="product-thumb">
+
+							<?php
+							/**
+							 * The Template for displaying all single posts
+							 *
+							 * @package WordPress
+							 * @subpackage Twenty_Fourteen
+							 * @since Twenty Fourteen 1.0
+							 */
+
+							get_sidebar(); ?>
+
+
+						</div> <!-- /.product-thumb -->
+						<div class="product-content overlay">
+
+						</div> <!-- /.product-content -->
+					</div> <!-- /.product-item-2 -->
+				</div> <!-- /.col-md-4 -->
+			</div> <!-- /.row -->
+		</div> <!-- /.container -->
+	</div> <!-- /.content-section -->
+
+
+
+
+
+
+
+
+
+				<div class="col-md-4">
+					<div class="footer-widget">
+						<h3 class="widget-title">Our Newsletter</h3>
+						<div class="newsletter">
+							<form action="#" method="get">
+								<p>Sign up for our regular updates to know when new products are released.</p>
+								<input type="text" title="Email" name="email" placeholder="Your Email Here">
+								<input type="submit" class="s-button" value="Submit" name="Submit">
+							</form>
+						</div> <!-- /.newsletter -->
+					</div> <!-- /.footer-widget -->
+				</div> <!-- /.col-md-4 -->
+			</div> <!-- /.row -->
+		</div> <!-- /.container -->
+	</div> <!-- /.main-footer -->
+
+
+
+
+
+
+
+
+
+	<div class="content-section">
+		<div class="container">
+			<div class="row">
+			<div class="col-md-8">
+
+		<div class="product-information">
+
+
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
@@ -22,8 +117,8 @@ get_header(); ?>
 					 */
 					get_template_part( 'content', get_post_format() );
 
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
+
+
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
@@ -31,7 +126,15 @@ get_header(); ?>
 					}
 				endwhile;
 			?>
+			</div>
+		</div>
+			</div>
+		</div>
+	</div>
+
+			
 		</div><!-- #content -->
+
 	</div><!-- #primary -->
 
 <?php
