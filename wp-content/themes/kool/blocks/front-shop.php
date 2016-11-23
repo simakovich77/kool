993c3db646170290fb9241e14585dc231a82789e
sim.i.a <simakovich77@gmail.com>
2016-11-23 14:37:34 +0300

    dfgggg11

--- wp-content/themes/kool/blocks/front-shop.php
+++ wp-content/themes/kool/blocks/front-shop.php
@@ -4,31 +4,36 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 section-title">
-                <h2>Shop</h2>
+                <h2>Products</h2>
             </div> <!-- /.section -->
         </div> <!-- /.row -->
         <div class="row">
 
-            <?php foreach ($postp as $posts) : ?>
+
 
                 <div class="col-md-3 col-sm-6">
+
+
                     <div class="product-item">
                         <div class="product-thumb">
-                            <?php
-                            //function do_shortcode([product_page id="1"]);
-                            ?>
-                            <?php echo get_the_post_thumbnail($posts); ?>
+
                         </div> <!-- /.product-thum -->
 
                         <div class="product-content">
-                            <
-                            <span class="price"><?=get_the_excerpt($posts); ?></span>
+
+                            <?php
+                            add_theme_support( 'woocommerce' );
+
+                            if ( shortcode_exists( 'product_categories' ) ) {
+                                echo do_shortcode('[recent_products per_page="3" columns="3"]');
+                            }
+                            ?>
 
                         </div> <!-- /.product-content -->
                     </div> <!-- /.product-item -->
                 </div> <!-- /.col-md-3 -->
 
-            <?php endforeach; ?>
+
 
         </div> <!-- /.row -->
     </div> <!-- /.container -->
